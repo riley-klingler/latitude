f1b80a38e845aa19bb519f6ddae805b364ba5d55
Desmond Brand <dmnd@desmondbrand.com>
2019-10-17 12:01:23 -0700

    💚 Actually fix CI

--- entryPoints.js
+++ entryPoints.js
@@ -85,7 +85,6 @@ module.exports = {
   DateRangeFilter: "filter/DateRangeFilter.jsx",
   Grid: "grid/Grid.jsx",
   DocumentTabs: "document/DocumentTabs/DocumentTabs.jsx",
-  F1InputError: "base_candidate/formula_one/F1InputError.jsx",
   // Secret menu! These modules don't seem to be documented but looked important
   // enough not to delete.
   CalendarDateRangeTools: "date/CalendarDateRangeTools.js",
