d710fe5834708920dd4bf68c62ed5f604ce0a06b
Shawn Chen <schen@flexport.com>
2019-10-21 09:49:07 -0700

    Deleting Design Systems folder

--- design_system/DesignSystemApp.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * TEAM: frontend_infra
- * @flow
- */
-
-import { hot } from "react-hot-loader";
-import DesignSystemRouter from "./DesignSystemRouter";
-
-export default hot(module)(DesignSystemRouter);
