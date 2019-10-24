f474e18768547d4db01e30ed0489041b0ad8043c
Bryan Manuele <bryan@flexport.com>
2019-10-24 16:12:33 -0700

    DatePicker gets styled (#52)

--- date/DatePicker.jsx
+++ date/DatePicker.jsx
@@ -8,19 +8,11 @@ import * as React from "react";
 import typeof DatePickerOverrideType from "./DatePickerOverride";
 
 const LazyDatePicker = React.lazy(() => {
-  // TODO(dmnd): Confusingly, one of these imports uses CSS loader despite the
-  // comment on the guarded import below...
   const imports = [
     import(/* webpackChunkName: "react-datepicker" */ "./DatePickerOverride"),
+    import(/* webpackChunkName: "react-datepicker" */ "../vendor_stylesheets/react-datepicker.css"),
     import(/* webpackChunkName: "react-datepicker" */ "./DatePickerStylesOverride.css"),
   ];
-
-  // if server side rendering, don't call CSS Loader
-  if (document) {
-    imports.push(
-      import(/* webpackChunkName: "react-datepicker" */ "../vendor_stylesheets/react-datepicker.css")
-    );
-  }
 
   return Promise.all(imports).then(([componentModule]) => componentModule);
 });
